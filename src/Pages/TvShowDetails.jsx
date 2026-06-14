aec365a03eb5c718327a3310c7e2eb2a6366041d
Jérôme Hudon <hudonj18@gmail.com>
2026-06-14 16:09:07 -0400

    ajout Tvshows player

--- src/Pages/TvShowDetails.jsx
+++ src/Pages/TvShowDetails.jsx
@@ -1,127 +1,149 @@
-import { api_key } from "../constants.js";
+import { api_key, dateFormatter, token } from "../constants.js";
 import { Link, useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { useContext, useState, useEffect, use } from "react";
-import Cards from "../components/Cards.jsx";
+import ShowcaseCards from "../components/ShowcaseCards.jsx";
+import CastCell from "../components/CastCell.jsx";
+import WatchTvShows from "../components/WatchTvShows.jsx";
 
 export function TvShowDetails() {
-	const [tvshow, setTvshow] = useState(null);
+	const [tvShow, setTvShow] = useState(null);
+	const [credits, setCredits] = useState(null);
 
-	const id = useParams("id");
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const streaming = searchParams.get("streaming") === "true";
+
+	const idName = useParams("id");
+	const id = idName.id.split("-")[0];
 
 	useEffect(() => {
-		async function getTvshow() {
-			const rep = await fetch(`/tmdb/tv/${id}?api_key=${api_key}`, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			if (rep.ok) {
-				const data = await rep.json();
-				setTvshow(data);
-			}
+		async function getData() {
+			const [tvShowRes, creditsRes] = await Promise.all([
+				fetch(`/tmdb/tv/${id}`, { headers: { Authorization: `Bearer ${token}` } }),
+				fetch(`/tmdb/tv/${id}/aggregate_credits`, {
+					headers: { Authorization: `Bearer ${token}` },
+				}),
+			]);
+			setTvShow(await tvShowRes.json());
+			setCredits(await creditsRes.json());
 		}
-		getTvshow();
-	}, [tvshowId]);
+		getData();
+	}, [id]);
+
+	useEffect(() => {
+		console.log(tvShow);
+	}, [tvShow]);
+
+	useEffect(() => {
+		console.log(credits);
+	}, [credits]);
 
 	return (
-		<div className="container">
-			<div className="card is-shadowless">
-				<div className="card-content">
-					<div className="columns is-variable is-4">
-						<div className="column is-narrow">
-							<figure className="image" style={{ width: 500 }}>
-								<img
-									src={show.imgUrl}
-									alt={show.title}
-									style={{
-										borderRadius: "var(--border-radius-md)",
-									}}
-								/>
-							</figure>
-						</div>
-
-						<div className="column">
-							<p className="title is-4 mb-1">{show.title}</p>
-							<p className="has-text-grey mb-2">{show.year}</p>
-
-							<div
-								className="is-flex is-align-items-center mb-2"
-								style={{ gap: "1rem" }}
-							>
-								<span className="tag is-size-6">{show.episodeCount} episodes</span>
-								<span className="tag is-size-6">{show.rating}</span>
-								<span className="ml-auto">
-									{show.genres.map((g) => g.name).join(", ")}
-								</span>
+		<>
+			<div className={"container is-fluid"} style={streaming ? { display: "none" } : {}}>
+				<div className="card is-shadowless">
+					<div className="card-content">
+						<div className="columns is-variable is-4">
+							<div className="column is-narrow">
+								<figure className="image" style={{ width: 420 }}>
+									<ShowcaseCards
+										key={tvShow?.id}
+										img={`https://image.tmdb.org/t/p/w500${tvShow?.poster_path}`}
+										to={`/tv/${tvShow?.id}-${tvShow?.name.replace(/\s+/g, "-").toLowerCase()}`}
+										vote_average={tvShow?.vote_average}
+									/>
+								</figure>
 							</div>
 
-							<div
-								className="is-flex is-align-items-center mb-3"
-								style={{ gap: "0.75rem" }}
-							>
-								<span className="has-text-grey">Studio</span>
-								<span>{show.studio.name}</span>
+							<div className="column">
+								<h1 className="title is-1 mb-1">{tvShow?.name}</h1>
+
+								<div
+									className="is-flex is-align-items-center mb-3"
+									style={{ gap: "2rem" }}
+								>
+									{tvShow?.production_companies.map((c) => (
+										<img
+											key={c.id}
+											src={`https://image.tmdb.org/t/p/w92${c.logo_path}`}
+											alt={c.name}
+											style={{
+												filter: c.logo_path.endsWith(".jpg")
+													? "none"
+													: "brightness(0) invert(1)",
+											}}
+										/>
+									))}
+								</div>
+
+								<p className="mb-4">
+									<i className="fa-regular fa-calendar-days"></i>{" "}
+									{tvShow?.first_air_date
+										? dateFormatter.format(new Date(tvShow.first_air_date))
+										: ""}{" "}
+									{tvShow?.origin_country ? `(${tvShow.origin_country[0]})` : ""}
+								</p>
+
+								<p className="mb-4">
+									<i class="fa-solid fa-tv"></i>{" "}
+									{tvShow?.number_of_seasons && tvShow?.number_of_episodes
+										? `${tvShow.number_of_seasons} seasons (${tvShow.number_of_episodes} episodes)`
+										: ""}{" "}
+								</p>
+
+								<div
+									className="is-flex is-align-items-center mb-3"
+									style={{ gap: "0.5rem", flexWrap: "wrap" }}
+								>
+									{tvShow?.genres.map((g) => (
+										<span
+											key={g.id}
+											className="tag is-size-6 is-rounded py-5 px-4"
+										>
+											{g.name}
+										</span>
+									))}
+								</div>
+
+								{/* <div
+									className="is-flex is-align-items-center mb-3"
+									style={{ gap: "1rem", flexWrap: "wrap" }}
+								>
+									{credits?.crew
+										.filter((c) => c.job === "Director")
+										.map((d) => (
+											<CastCell
+												key={d.id}
+												name={d.name}
+												profile_path={d.profile_path}
+												role={d.job}
+											/>
+										))}
+								</div> */}
+
+								<div className="mb-5">
+									<button
+										className="tag is-size-6 is-rounded py-5 px-6 is-link"
+										style={{ gap: "0.5rem" }}
+										onClick={() => setSearchParams({ streaming: true })}
+									>
+										<i className="fa-solid fa-play"></i> Watch
+									</button>
+								</div>
+
+								<div className="title is-3" style={{ marginTop: "2rem" }}>
+									Overview
+								</div>
+
+								<p>{tvShow?.overview}</p>
 							</div>
-
-							<p>{show.plot}</p>
-
-							<audio controls style={{ marginTop: "2rem" }}>
-								<source src={show.audioUrl} type="audio/mpeg" />
-								Your browser does not support the audio element.
-							</audio>
 						</div>
 					</div>
 				</div>
 			</div>
 
-			{/* <div className="card is-shadowless">
-				<div className="card-content">
-					<div
-						className="columns"
-						style={{
-							overflowX: "auto",
-							flexWrap: "nowrap",
-							marginTop: "2rem",
-						}}
-					>
-						{tvshow?.roles.map((role) => (
-							<Cards
-								img={role.imgUrl}
-								description={role.character}
-								title={role.name}
-								size="2"
-								fontSize="0.8rem"
-								key={role.roleId}
-							/>
-						))}
-					</div>
-				</div>
-			</div> */}
-
-			{/* <div className="card is-shadowless">
-				<div className="card-content">
-					<div
-						className="row columns is-multiline is-mobile"	
-						style={{
-							marginTop: "2rem",
-						}}
-					>
-						{tvshow?.seasons.map((season) => (
-							<Cards
-								img={season.imgUrl}
-								description={season.episodeCount + " episodes"}
-								title={"Season " + season.number}
-								size="4"
-								fontSize="2em"
-								key={season.seasonId}
-								to={"/saison?seasonId=" + season.seasonId}
-							/>
-						))}
-					</div>
-				</div>
-			</div> */}
-		</div>
+			{streaming && <WatchTvShows tmdbId={id} />}
+		</>
 	);
 }
 
---
+++ src/components/WatchTvShows.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Link, useNavigate, useParams, useSearchParams } from "react-router-dom";
+
+const url = "https://player.videasy.to/tv/";
+const url1 = "https://111movies.net/tv/";
+
+function WatchTvShows({ tmdbId, season, episode }) {
+    const [source, setSource] = useState(url);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    return (
+        <div className="container">
+            <div className="mt-5 mb-5" style={{ position: "relative" }}>
+                <div
+                    className="button is-dark"
+                    onClick={() => {
+                        searchParams.delete("streaming");
+                        setSearchParams(searchParams);
+                    }}
+                    type="button"
+                    style={{ position: "absolute", top: 0, left: 0 }}
+                >
+                    <span className="icon">
+                        <i className="fa-solid fa-arrow-left" />
+                    </span>
+                </div>
+
+                <div className="is-flex is-justify-content-center">
+                    <button
+                        onClick={() => setSource(source === url ? url1 : url)}
+                        className="button is-link"
+                    >
+                        {source === url ? "Switch to 111movies" : "Switch to Videasy"}
+                    </button>
+                </div>
+            </div>
+
+            <div
+                className="is-flex is-justify-content-center is-align-items-center is-flex-direction-column"
+                style={{ width: "100%" }}
+            >
+                <iframe
+                    src={`${source}${tmdbId}/${season}/${episode}`}
+                    style={{ width: "100%", aspectRatio: "16/9", border: "none" }}
+                    allowFullScreen
+                />
+            </div>
+        </div>
+    );
+}
+
+export default WatchTvShows;
